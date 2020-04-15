f2f9cbfbe86076f7d4bce2bd570bc96de2c1cca6
Есбол Молдрахметов <es@MacBook-Air-Esbol.local>
2020-04-16 03:53:27 +0600

    debug false

--- icj/settings.py
+++ icj/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'main/templates/main'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
