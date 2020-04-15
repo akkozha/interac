7cb22cee1726be5a282b821fc17b5702ab4be1cd
Есбол Молдрахметов <es@MacBook-Air-Esbol.local>
2020-04-16 02:39:11 +0600

    ICJ-9 update setting template

--- icj/settings.py
+++ icj/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'icj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'main/templates/main'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
