a7c656728feea87ee5fa5e6bb98d531a24d862a7
Есбол Молдрахметов <es@MacBook-Air-Esbol.local>
2020-04-19 20:15:15 +0600

    add quiz app

--- deployment/quiz/views.py
+++ deployment/quiz/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'quiz/index.html', {})
-
-def quiz(request, num=1):
-    return render(request, 'quiz/quiz-{}.html'.format(num), {})
